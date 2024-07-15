e6f7b788bb75f954ec44f385c577278f222a549c
Brody Brooks <brody@brodybrooks.com>
2024-07-15 13:03:41 -0700

    Moved watermark images, added start of gschema/gnome settings

--- config/scripts/removeBluefinWallpapers.sh
+++ config/scripts/removeBluefinWallpapers.sh
@@ -12,3 +12,13 @@ rm -f /usr/share/wallpapers/xe_clouds.jpeg
 rm -f /usr/share/wallpapers/xe_foothills.jpeg
 rm -f /usr/share/wallpapers/xe_space_needle.jpeg
 rm -f /usr/share/wallpapers/xe_sunset.jpeg
+
+rm -f /usr/share/gnome-background-properties/bluefin-autumn-dynamic.xml
+rm -f /usr/share/gnome-background-properties/bluefin-sprint-dynamic.xml
+rm -f /usr/share/gnome-background-properties/bluefin-summer-dynamic.xml
+rm -f /usr/share/gnome-background-properties/bluefin-winter-dynamic.xml
+rm -f /usr/share/gnome-background-properties/chicken.xml
+rm -f /usr/share/gnome-background-properties/xe_clouds.xml
+rm -f /usr/share/gnome-background-properties/xe_foothills.xml
+rm -f /usr/share/gnome-background-properties/xe_space_needle.xml
+rm -f /usr/share/gnome-background-properties/xe_sunset.xml
