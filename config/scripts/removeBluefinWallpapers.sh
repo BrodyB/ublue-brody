598a29ac4681f42c3da5f9378721fad89749da84
Brody Brooks <brody@brodybrooks.com>
2024-07-15 12:17:40 -0700

    Added watermarks, added script to remove bluefin wallpapers

---
+++ config/scripts/removeBluefinWallpapers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+rm -rf /usr/share/backgrounds/bluefin
+rm -f /usr/share/backgrounds/chicken.webp
+rm -f /usr/share/backgrounds/xe_clouds.jpeg
+rm -f /usr/share/backgrounds/xe_foothills.jpeg
+rm -f /usr/share/backgrounds/xe_space_needle.jpeg
+rm -f /usr/share/backgrounds/xe_sunset.jpeg
+rm -f /usr/share/wallpapers/xe_clouds.jpeg
+rm -f /usr/share/wallpapers/xe_foothills.jpeg
+rm -f /usr/share/wallpapers/xe_space_needle.jpeg
+rm -f /usr/share/wallpapers/xe_sunset.jpeg
